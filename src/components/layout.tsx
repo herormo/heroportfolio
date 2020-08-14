3cc83a087bcd69562f9040ad658c51175c4d7511
HeroMohammed <herorfaat.mohammed@gmail.com>
2020-08-14 23:34:31 +0300

    flex footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -38,10 +38,9 @@ const Layout: React.FC<Props> = ({ children }) => {
       <Header menuLinks={headerData.menuLinks("")} siteTitle={headerData.title("")} />
       <div
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
+          display: `flex`,
+          flexDirection:`column`,
+          minHeight:`100vh`,
         }}
       >
         <main>{children}</main>
